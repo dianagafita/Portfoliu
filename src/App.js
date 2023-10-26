f1db1517d7a18c307946182de30efba747c17db9
dianagafita <gafita.diana12@gmail.com>
2023-10-26 14:47:42 +0300

    commit the modified header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import { AnimatePresence } from "framer-motion";
 import About from "./components/About";
-import BasicExample from "./components/Contact";
 import Contact from "./components/Contact";
 import Header from "./components/Header/Header";
 import Home from "./components/Home";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+
 function App() {
   return (
     <>
       <Header />
-
       <main>
         <Home />
         <About />
